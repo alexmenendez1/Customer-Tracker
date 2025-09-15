e28533c53bc9a6dc5cbb2af3398d5833b825f45e
Alex Menendez <amenendez15@usf.edu>
2025-09-14 22:55:45 -0400

    onto step 5. trying to work it out

--- cc_3.js
+++ cc_3.js
@@ -4,11 +4,16 @@ let customers = ["Eren Yeager","erenyeager@gmail.com", ["Sword Set", "ODM Gear"]
     ["Mikasa Ackermann", "mikasaackermann@gmail.com", ["Sword Set", "ODM Gear"]],
    ["Hange Zoe", "hangezoe@gmail.com",["Sword Set ", "ODM Gear"]];
 
-customers.forEach(obj=> console.log(`${obj.sku} \t|\t ${obj.name}`))
+
 customers.push("Reiner Braun", "reinerbraun@gmail.com", ["Sword Set", "ODM Gear"])
 xyz= customers.shift()
 console.log(xyz)
 
 customers[1]= 'arminarlert@gmail.com'
 console.log(customers)
-newPurchase= customerspush()
+newPurchase= customers[2]
+newPurchase.push("Scout Regiment Robe")
+console.log(newPurchase)
+
+customers.forEach(obj=> console.log(`${customers}))
+    
